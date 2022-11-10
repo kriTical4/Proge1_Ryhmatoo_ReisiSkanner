5abc470c8ddde901f0b7c956b2051fe0a723d894
Kristian Tamm <tammkristian@gmail.com>
2022-11-10 22:30:06 +0200

    algversioon

--- projekt.py
+++ projekt.py
@@ -1,9 +1,25 @@
-def parim_pilet(riik, aeg):
-    #brrr(momondo API/web scrape)
-    #tagastab lingi lennu broneerimiseks
+import requests
+import bs4
+def aita_koodi_tagastja(linn):
+    try:
+        lehe_nimi = "https://www.iata.org/en/publications/directories/code-search/?airport.search=" + linn
+        res = requests.get(lehe_nimi)
+        res.raise_for_status()
+        iata_soup = bs4.BeautifulSoup(res.text, "html.parser")
+        koodid = iata_soup.select(".datatable")
+        scrape_list = str(koodid).split("\n")
+        aita_kood = scrape_list[12].replace("<td>", "")
+        aita_kood = aita_kood.replace("</td>", "")
+        return aita_kood
+    except:
+        return False
     
-def main():
-    print("Tere Tamm!")
-
-main():
-    
+def parim_pilet(sihtkoha_kood, algus_kuupäev, lõpp_kuupäev):
+    lähtekoha_kood = "TLL"
+    #kuupäeva sisendid on juba YYYY-MM-DD vormis
+    aadress = "https://www.momondo.ee/flight-search/"+lähtekoha_kood+"-"+sihtkoha_kood+"/"+algus_kuupäev+"/"+lõpp_kuupäev+"?sort=bestflight_a"
+    res = requests.get(aadress)
+    res.raise_for_status()
+    pilet_soup = bs4.BeautifulSoup(res.text, "html.parser")
+    hinnad = pilet_soup.find_all("span", class_="price option-text")
+    return str(hinnad)
